5ffcec9a6ee2d130891854d169ec1572c1b52f6c
dzefa <danchong625@gmail.com>
2019-01-26 12:01:52 -0800

    [util] adds db logger to separate txt file on prod

--- src/util/logger.ts
+++ src/util/logger.ts
@@ -1,5 +1,20 @@
+import fs from 'fs';
+import path from 'path';
+
 export const log = (...args: string[] | any[]) => {
   if (process.env.NODE_ENV === 'dev') {
     console.log(...args);
+  }
+};
+
+export const dbLog = (...args: string[] | any[]) => {
+  const pathToFile = path.resolve(__dirname, '../../dist/log/dbLog.txt');
+
+  if (process.env.NODE_ENV === 'production') {
+    fs.appendFile(pathToFile, JSON.stringify([...args]), (err) => {
+      if (err) {
+        console.log(err);
+      }
+    });
   }
 };
