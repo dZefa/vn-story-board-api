1b1419ecac6064efde82305ba8ed8b83c2d69b62
dzefa <danchong625@gmail.com>
2019-01-26 20:58:40 -0800

    [guard] refactors and adds user login case

--- src/middlewares/body.guard.ts
+++ src/middlewares/body.guard.ts
@@ -1,19 +1,31 @@
 import { RequestHandler, Response, Request, NextFunction } from 'express';
 
-import { UserAddModel } from '../database/models/user';
+import { UserAddModel, UserLoginModel } from '../database/models/user';
 
 export const bodyGuard: ((type: string) => RequestHandler) = ((type) => (req: Request, res: Response, next: NextFunction) => {
+  let body: UserAddModel | UserLoginModel | any;
+  let isValid: boolean;
+
   switch (type) {
-    case 'user':
-      const body: UserAddModel = req.body;
-      const isValid: boolean = body.email && body.password && body.username && true;
+    case 'user-register':
+      body = req.body as UserAddModel;
+      isValid = !!body.email && !!body.password && !!body.username;
 
       if (!isValid) {
-        return res.status(400).send({ result: { error: 'MISSING_INFORMATION' }})
+        return res.status(400).send({ result: { error: 'MISSING_INFORMATION' } });
       }
 
       break;
+    case 'user-login':
+      body = req.body as UserLoginModel;
+      isValid = !!body.username && !!body.password;
+
+      if (!isValid) {
+        return res.status(400).send({ result: { error: 'MISSING_INFORMATION' } });
+      }
     default: 
-      next();
+      break;
   };
+
+  next();
 });
