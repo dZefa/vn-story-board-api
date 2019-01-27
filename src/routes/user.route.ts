e5091922e5d14f72f52e511a9c0a6146609bb74b
dzefa <danchong625@gmail.com>
2019-01-26 20:59:11 -0800

    [feat] adds user login route

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -2,19 +2,27 @@ import { Router, Response, Request } from 'express';
 
 import { bodyGuard } from '../middlewares/body.guard';
 
-import { UserAddModel } from 'src/database/models/user';
+import { UserAddModel, UserLoginModel } from 'src/database/models/user';
 
 import { UserService } from '../service/user.service';
 
 const userRouter: Router = Router();
 const userService = new UserService();
 
-userRouter.post('/register', bodyGuard('user'), async (req: Request, res: Response) => {
+userRouter.post('/register', bodyGuard('user-register'), async (req: Request, res: Response) => {
   const payload: UserAddModel = req.body;
 
   const user = await userService.register(payload);
 
   res.status(201).send({ result: { user }});
+});
+
+userRouter.post('/login', bodyGuard('user-login'), async (req: Request, res: Response) => {
+  const payload: UserLoginModel = req.body;
+
+  const token = await userService.login(payload);
+
+  res.status(200).send({ result : { token } });
 });
 
 export { userRouter };
