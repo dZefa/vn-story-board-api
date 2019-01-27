0319e6776e8fef0525c135d340b026159a4004c0
dzefa <danchong625@gmail.com>
2019-01-26 20:34:32 -0800

    [route] implements body guard middleware to user register

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,5 +1,7 @@
 import { Router, Response, Request } from 'express';
 
+import { bodyGuard } from '../middlewares/body.guard';
+
 import { UserAddModel } from 'src/database/models/user';
 
 import { UserService } from '../service/user.service';
@@ -7,7 +9,7 @@ import { UserService } from '../service/user.service';
 const userRouter: Router = Router();
 const userService = new UserService();
 
-userRouter.post('/register', async (req: Request, res: Response) => {
+userRouter.post('/register', bodyGuard('user'), async (req: Request, res: Response) => {
   const payload: UserAddModel = req.body;
 
   const user = await userService.register(payload);
